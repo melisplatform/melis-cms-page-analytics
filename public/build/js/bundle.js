e801083cdd8a3cd5540167bb62beab7e2ce1053b
Junry Zabate <jzabate@melistechnology.com>
2019-12-04 15:52:50 +0800

    fixes for data table pagination

--- public/build/js/bundle.js
+++ public/build/js/bundle.js
@@ -21,27 +21,22 @@ var _pageId = null;
         d.pageId = activeTabId.split("_")[0];
     }
 
-    // paginate dataTables data
-    window.paginateDataTables = function() {
-        melisCore.paginateDataTables();
-    }
-
 $(function() {
     var $body       = $("body"),
         zoneId      = "",
         melisKey    = "";
 
-    //For Opening to the other tab
-    $body.on('click' , "#page_hit_view" , function(){
-            var $this   = $(this),
-                tabName = $this.closest('tr').find("td:nth-child(3)").text() + " / Page Analytics",
-                pageId  = activeTabId.split("_")[0];
-                
-                toolPageDetails.tabOpen(tabName, pageId);
-                _pageId = pageId;
-    });
+        //For Opening to the other tab
+        $body.on('click' , "#page_hit_view" , function(){
+                var $this   = $(this),
+                    tabName = $this.closest('tr').find("td:nth-child(3)").text() + " / Page Analytics",
+                    pageId  = activeTabId.split("_")[0];
+                    
+                    toolPageDetails.tabOpen(tabName, pageId);
+                    _pageId = pageId;
+        });
 
-    //this will do the initialization of the page_details
+        //this will do the initialization of the page_details
         var toolPageDetails = {
             tabOpen: function(name, id){
                 melisHelper.tabOpen(name, 'fa fa-list-ul', id+'_id_meliscms_page_analytics_page_details', 'meliscms_page_analytics_page_details', { pageHitId : id , tabName : name});
